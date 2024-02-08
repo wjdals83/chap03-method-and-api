637c33f5b616b0814119af78085fde2bdab0d206
jungmin <tjwjdals0803@naver.com>
2024-02-08 17:55:18 +0900

    2월 5, 6. 7, 8일 method, api 수업내용

--- src/main/java/com/ohgiraffers/section04/scanner/Application02.java
+++ src/main/java/com/ohgiraffers/section04/scanner/Application02.java
@@ -6,7 +6,7 @@ public class Application02 {
 
     public static void main(String[] args) {
 
-        /* 수업목표. scanner 의 nextLone() 과 next 에 대해 구분하여 사용할 수 있다. */
+        /* 수업목표. scanner 의 nextLine() 과 next 에 대해 구분하여 사용할 수 있다. */
         /* 필기.
         *   nextLine() : 공백을 포함한 한 줄을 입력을 위한 개행문자 전 까지 읽어서 문자열로 반환한다. (공백문자 포함)
         *   next() : 공백문자나 개행문자 전 까지를 읽어서 문자열로 반환한다. (공백문자 포함하지 않음)
